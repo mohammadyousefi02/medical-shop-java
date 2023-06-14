937b91791bbe2028c17f48fe09246e966a82fdb1
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-06-14 09:52:20 +0330

    create product services repository & controller

---
+++ src/main/java/com/example/medicalshop/product/ProductService.java
@@ -0,0 +1,29 @@
+package com.example.medicalshop.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+}
