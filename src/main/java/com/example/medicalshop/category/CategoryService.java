4532398d0a93b5ff46254db5aec4d985c491ed63
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-06-14 09:52:40 +0330

    create Category services repository & controller

---
+++ src/main/java/com/example/medicalshop/category/CategoryService.java
@@ -0,0 +1,25 @@
+package com.example.medicalshop.category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
+}
